2ace0215b62375f1c8763b7067dd60ca72c76db6
Dhruv Chikhaliya <dhruvchikhaliya12@gmail.com>
2023-01-29 14:25:24 +0530

    Ultrasonic sensor distance measurement

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@
 #include <PumpLightService.h>
 
 #define SERIAL_BAUD_RATE 115200
-#define SERIAL2_BAUD_RATE 9200
+#define SERIAL2_BAUD_RATE 9600
 
 int water_level = 2000;
 
@@ -31,27 +31,28 @@ TaskHandle_t Task2;
 
 void Services(void* pvParameters);
 void TankController(void* pvParameters);
+int getDistance();
 
 void setup() {
   // start serial and filesystem
   Serial.begin(SERIAL_BAUD_RATE);
   Serial2.begin(SERIAL2_BAUD_RATE);
 
-  // start the framework and demo project
-  esp8266React.begin();
+  // // start the framework and demo project
+  // esp8266React.begin();
 
-  // Pump
-  pumpSettingService.begin();
-  pumpStartStopPointService.begin();
+  // // Pump
+  // pumpSettingService.begin();
+  // pumpStartStopPointService.begin();
 
-  // // load the initial light settings
-  // lightStateService.begin();
+  // // // load the initial light settings
+  // // lightStateService.begin();
 
-  // // start the light service
-  // lightMqttSettingsService.begin();
+  // // // start the light service
+  // // lightMqttSettingsService.begin();
 
-  // start the server
-  server.begin();
+  // // start the server
+  // server.begin();
 
   // xTaskCreatePinnedToCore(Services, "Task1", 10000, NULL, 1, &Task1, 0);
   // delay(500);
@@ -72,8 +73,30 @@ void TankController(void* pvParameters) {
 }
 
 void loop() {
-  esp8266React.loop();
+  // esp8266React.loop();
   if (Serial2.available()) {
-    Serial.println(Serial2.read());
+    water_level = getDistance();
   }
+}
+int getDistance() {
+  unsigned int distance;
+  byte startByte, h_data, l_data, sum = 0;
+  byte buf[3];
+
+  startByte = (byte)Serial2.read();
+  if (startByte == 255) {
+    Serial2.readBytes(buf, 3);
+    h_data = buf[0];
+    l_data = buf[1];
+    sum = buf[2];
+    distance = (h_data << 8) + l_data;
+    if (((h_data + l_data)) != sum) {
+    } else {
+      return distance;
+    }
+  }  
+
+  else
+    return;
+  delay(100);
 }
